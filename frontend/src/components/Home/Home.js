f9e5275d43e176b28d7ed87b677ba80aa86c55a2
Ericarthurc <hebrew.8.12@gmail.com>
2020-05-13 09:09:35 -0700

    Update for Awards

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -7,13 +7,77 @@ import { Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 import chapel_5_7 from '../../assets/images/chapel_5-7.png';
 import chapel_4_30 from '../../assets/images/chapel_4-30.png';
 import pe_e_1 from '../../assets/images/pe_e_1.png';
-import pe_m_1 from '../../assets/images/pe_m_1.png';
 import pe_2 from '../../assets/images/pe_2.png';
+
+import writers from '../../assets/images/writers.png';
+import kinder2nd from '../../assets/images/kinder2nd.png';
+import third5th from '../../assets/images/3rd5th.png';
+import sixth8th from '../../assets/images/6th8th.png';
 
 const Home = () => {
   return (
     <>
       <div className="Main__Content-Wrapper">
+        <div className="Content-Wrapper">
+          <p className="Content-Header">
+            Creative Writing and Art Contest Awards
+          </p>
+          <div className="Content__Video-Wrapper">
+            <Link
+              to={`/watch?v=JanetteWritersAwards&til=Awards&tec=Mrs.%20McLaughlin`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={writers}
+                ></img>
+                <div className="Video-Context">Writers' Awards</div>
+              </div>
+            </Link>
+            <Link
+              to={`/watch?v=JillAwardsforKinder-2nd&til=Awards%20for%20Kinder-2nd&tec=Mrs.%20Isaac`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={kinder2nd}
+                ></img>
+                <div className="Video-Context">Awards for Kinder-2nd</div>
+              </div>
+            </Link>
+          </div>
+          <div style={{ marginTop: '35px' }}></div>
+          <div className="Content__Video-Wrapper">
+            <Link
+              to={`/watch?v=JillAwardsfor3rd-5th&til=Awards%20for%203rd-5th&tec=Mrs.%20Isaac`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={third5th}
+                ></img>
+                <div className="Video-Context">Awards for 3rd-5th</div>
+              </div>
+            </Link>
+            <Link
+              to={`/watch?v=JillAwardsfor6th-8th&til=Awards%20for%206th-8th&tec=Mrs.%20Isaac`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={sixth8th}
+                ></img>
+                <div className="Video-Context">Awards for 6th-8th</div>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        {/* SPACER */}
         <div className="Content-Wrapper">
           <p className="Content-Header">Chapel Videos</p>
           <div className="Content__Video-Wrapper">
@@ -41,8 +105,10 @@ const Home = () => {
             </Link>
           </div>
         </div>
+
+        {/* SPACER */}
         <div className="Content-Wrapper">
-          <p className="Content-Header">Physical Education: Elementary</p>
+          <p className="Content-Header">Physical Education</p>
           <div className="Content__Video-Wrapper">
             <Link to={`/watch?v=pe_2&til=PE&tec=Dani%20Fraley`}>
               <div className="Content__Video-Thumbnail-Wrapper">
@@ -51,7 +117,7 @@ const Home = () => {
                   alt="chapel_logo"
                   src={pe_2}
                 ></img>
-                <div className="Video-Context">Elementary PE (May 4th)</div>
+                <div className="Video-Context">PE (May 4th)</div>
               </div>
             </Link>
             <Link to={`/watch?v=ElementaryPE_1&til=PE&tec=Dani%20Fraley`}>
@@ -66,7 +132,7 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="Content-Wrapper">
+        {/* <div className="Content-Wrapper">
           <p className="Content-Header">Physical Education: Middle School</p>
           <div className="Content__Video-Wrapper">
             <Link to={`/watch?v=pe_2&til=PE&tec=Dani%20Fraley`}>
@@ -92,7 +158,7 @@ const Home = () => {
               </div>
             </Link>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
