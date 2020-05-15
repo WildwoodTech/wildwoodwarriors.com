5aeed308dc9db11c1d9fa02bb52810f9c7222008
Ericarthurc <hebrew.8.12@gmail.com>
2020-05-14 17:25:08 -0700

    Update 5:25pm, 5/14/2020

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -6,7 +6,7 @@ import { Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 // VIDEO THIMBNAILS
 import chapel_5_7 from '../../assets/images/chapel_5-7.png';
 import chapel_5_14 from '../../assets/images/chapel_5-14.png';
-import pe_e_1 from '../../assets/images/pe_e_1.png';
+import pe_3 from '../../assets/images/pe_3.png';
 import pe_2 from '../../assets/images/pe_2.png';
 
 import writers from '../../assets/images/writers.png';
@@ -110,6 +110,16 @@ const Home = () => {
         <div className="Content-Wrapper">
           <p className="Content-Header">Physical Education</p>
           <div className="Content__Video-Wrapper">
+            <Link to={`/watch?v=pe_3&til=PE&tec=Dani%20Fraley`}>
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={pe_3}
+                ></img>
+                <div className="Video-Context">PE (May 14th)</div>
+              </div>
+            </Link>
             <Link to={`/watch?v=pe_2&til=PE&tec=Dani%20Fraley`}>
               <div className="Content__Video-Thumbnail-Wrapper">
                 <img
@@ -120,45 +130,8 @@ const Home = () => {
                 <div className="Video-Context">PE (May 4th)</div>
               </div>
             </Link>
-            <Link to={`/watch?v=ElementaryPE_1&til=PE&tec=Dani%20Fraley`}>
-              <div className="Content__Video-Thumbnail-Wrapper">
-                <img
-                  className="Content__Video-Thumbnail"
-                  alt="chapel_logo"
-                  src={pe_e_1}
-                ></img>
-                <div className="Video-Context">Elementary PE (April 27th)</div>
-              </div>
-            </Link>
           </div>
         </div>
-        {/* <div className="Content-Wrapper">
-          <p className="Content-Header">Physical Education: Middle School</p>
-          <div className="Content__Video-Wrapper">
-            <Link to={`/watch?v=pe_2&til=PE&tec=Dani%20Fraley`}>
-              <div className="Content__Video-Thumbnail-Wrapper">
-                <img
-                  className="Content__Video-Thumbnail"
-                  alt="chapel_logo"
-                  src={pe_2}
-                ></img>
-                <div className="Video-Context">Middle School PE (May 4th)</div>
-              </div>
-            </Link>
-            <Link to={`/watch?v=MiddleschoolPE_1&til=PE&tec=Dani%20Fraley`}>
-              <div className="Content__Video-Thumbnail-Wrapper">
-                <img
-                  className="Content__Video-Thumbnail"
-                  alt="chapel_logo"
-                  src={pe_m_1}
-                ></img>
-                <div className="Video-Context">
-                  Middle School PE (April 27th)
-                </div>
-              </div>
-            </Link>
-          </div>
-        </div> */}
       </div>
     </>
   );
