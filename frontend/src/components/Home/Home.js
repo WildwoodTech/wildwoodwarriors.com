d47528db18f94677629fb38e4db8e5cf8b915089
Ericarthurc <hebrew.8.12@gmail.com>
2020-05-28 15:46:55 -0700

    updating for graduation

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -16,10 +16,71 @@ import sixth8th from '../../assets/images/6th8th.png';
 
 import wcaInterview from '../../assets/images/wca_interview.png';
 
+import k1 from '../../assets/images/k-1.png';
+import twothree from '../../assets/images/2-3.png';
+import fourfive from '../../assets/images/4-5.png';
+import sixeight from '../../assets/images/6-8.png';
+
 const Home = () => {
   return (
     <>
       <div className="Main__Content-Wrapper">
+        <div className="Content-Wrapper">
+          <p className="Content-Header">Graduation Ceremonies</p>
+          <div className="Content__Video-Wrapper">
+            <Link
+              to={`/watch?v=grad-k-1&til=Graduation%20for%20Kinder-1st&tec=Principal%20Reid`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={k1}
+                ></img>
+                <div className="Video-Context">Graduation K-1</div>
+              </div>
+            </Link>
+            <Link
+              to={`/watch?v=grad-2-3&til=Graduation%20for%202nd-3rd&tec=Principal%20Reid`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={twothree}
+                ></img>
+                <div className="Video-Context">Graduation 2-3</div>
+              </div>
+            </Link>
+          </div>
+          <div style={{ marginTop: '35px' }}></div>
+          <div className="Content__Video-Wrapper">
+            <Link
+              to={`/watch?v=grad-4-5&til=Graduation%20for%204rd-5th&tec=Principal%20Reid`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={fourfive}
+                ></img>
+                <div className="Video-Context">Graduation 4-5</div>
+              </div>
+            </Link>
+            <Link
+              to={`/watch?v=grad-6-7-8&til=Graduation%20for%206th-8th&tec=Principal%20Reid`}
+            >
+              <div className="Content__Video-Thumbnail-Wrapper">
+                <img
+                  className="Content__Video-Thumbnail"
+                  alt="chapel_logo"
+                  src={sixeight}
+                ></img>
+                <div className="Video-Context">Graduation 6-7-8</div>
+              </div>
+            </Link>
+          </div>
+        </div>
         {/* SPACER */}
         <div className="Content-Wrapper">
           <p className="Content-Header">Graduation Congratulations!</p>
